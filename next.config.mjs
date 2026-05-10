5e126239045ec1e89ce96b834a53565e30f09d20
Blake Gilman <blakegilman@MacBook-Air-235.local>
2026-05-09 22:12:41 -0400

    Configure server build workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS === "true"
-const repositoryName = "v0-finbro-dashboard"
-
 const nextConfig = {
-  output: isGithubActions ? "export" : undefined,
-  basePath: isGithubActions ? `/${repositoryName}` : "",
-  assetPrefix: isGithubActions ? `/${repositoryName}/` : "",
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
